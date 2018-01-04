822afae17239abf7f4e74bf304491e7ad9d3fe29
emzter <im_aem-ss@hotmail.com>
2018-01-04 22:56:21 +0700

    TEST

--- app/controller/utils/email.php
+++ app/controller/utils/email.php
@@ -3,7 +3,6 @@ namespace Controller\Utils;
 
 use Config\Database;
 use Controller\User\Profile;
-use Controller\PHPMailer\PHPMailer;
 
 class Email {
 
@@ -14,33 +13,43 @@ class Email {
         $emailTitle = Email::createTitle($type);
         $emailBody = Email::createEmail($type);
 
-        $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-        try {
-            //Server settings
-            $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-            $mail->isSMTP();                                      // Set mailer to use SMTP
-            $mail->Host = 'pathfinder.in.th';  // Specify main and backup SMTP servers
-            $mail->SMTPAuth = true;                               // Enable SMTP authentication
-            $mail->Username = 'contact@pathfinder.in.th';                 // SMTP username
-            $mail->Password = 'AemzaLanla159';                           // SMTP password
-            $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-            $mail->Port = 465;                                    // TCP port to connect to
+        $headers = 'Content-type: text/html; charset=UTF-8' . "\r\n" .
+        'From: PathFinder <contact@'.SITE_DOMAIN.'>';
+
+        return mail(
+            $user['email'],
+            $emailTitle,
+            $emailBody,
+            $headers
+        );
+
+        // $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+        // try {
+        //     //Server settings
+        //     $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        //     $mail->isSMTP();                                      // Set mailer to use SMTP
+        //     $mail->Host = 'pathfinder.in.th';  // Specify main and backup SMTP servers
+        //     $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        //     $mail->Username = 'contact@pathfinder.in.th';                 // SMTP username
+        //     $mail->Password = 'AemzaLanla159';                           // SMTP password
+        //     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        //     $mail->Port = 465;                                    // TCP port to connect to
         
-            //Recipients
-            $mail->setFrom('contact@pathfinder.in.th', 'PathFinder');
-            $mail->addAddress($user['email']);
+        //     //Recipients
+        //     $mail->setFrom('contact@pathfinder.in.th', 'PathFinder');
+        //     $mail->addAddress($user['email']);
         
-            //Content
-            $mail->isHTML(true);                                  // Set email format to HTML
-            $mail->Subject = $emailTitle;
-            $mail->Body    = $emailBody;
+        //     //Content
+        //     $mail->isHTML(true);                                  // Set email format to HTML
+        //     $mail->Subject = $emailTitle;
+        //     $mail->Body    = $emailBody;
         
-            $mail->send();
-            echo 'Message has been sent';
-        } catch (Exception $e) {
-            echo 'Message could not be sent.';
-            echo 'Mailer Error: ' . $mail->ErrorInfo;
-        }
+        //     $mail->send();
+        //     echo 'Message has been sent';
+        // } catch (Exception $e) {
+        //     echo 'Message could not be sent.';
+        //     echo 'Mailer Error: ' . $mail->ErrorInfo;
+        // }
     }
 
     public static function createEmail($type){
